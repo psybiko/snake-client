35c81f74278ea7d64e0eb53a03a4e6217753ca57
Kc Kai <kirstenckai@gmail.com>
2020-03-11 19:21:09 +0000

    Add ctrl + c encoding

--- play.js
+++ play.js
@@ -1,9 +1,32 @@
 /* 
-  *Estabilishes a connection with the game server
-*/
-const { connect } = require('./client');
+ *Estabilishes a connection with the game server
+ */
+const {
+  connect
+} = require('./client');
 console.log('Connecting...');
 connect();
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+  return stdin;
+}
 
-
+/* 
+ *Handle user input
+ * This allows us to terminate the app easily using ctrl + c
+ */
+const handleUserInput = function () {
+}
